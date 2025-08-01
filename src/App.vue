e52700ea78a3dd925161d9a9ed8f2a0ec1a5cb51
savio <saviopf22@gmail.com>
2025-08-01 15:39:44 -0300

    testing state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Calculator from "./components/Calculator.vue";
 </script>
 
 <template>
   <div class="flex flex-col h-screen w-full bg-gray-50">
     <HelloWorld msg="Vite + Vue" />
+    <Calculator />
   </div>
 </template>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref, watch, watchEffect } from "vue";
+const count = ref(0);
+
+// useEffect style
+onMounted(() => {
+  console.log(`Hello :)`);
+});
+onUnmounted(() => {
+  console.log(`Goodbye :)`);
+});
+
+// useEffect with automatic dependencies
+watchEffect(() => {
+  console.log(count.value);
+});
+
+// useEffect with dependencies
+watch(count, (newValue) => {
+  console.log(`Count changed to: ${newValue}`);
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 items-center justify-center h-full">
+    <h1 class="text-2xl font-bold">Counter: {{ count }}</h1>
+    <input type="number" v-model="count" class="p-2 border rounded" />
+    <button class="p-2 bg-blue-500 text-white rounded" @click="count++">
+      Increment
+    </button>
+  </div>
+</template>
